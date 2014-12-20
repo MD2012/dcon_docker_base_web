388c37b398384d3abc324a144f8be761ff80ab8b
MD <md@doulis.com>
2014-12-20 23:33:46 +0100

    update

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-// Constants
-var PORT = 8080;
-
-// App
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world 2\n');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
