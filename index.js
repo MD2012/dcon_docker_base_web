05ac657790900e0063ee334f260a25c926e1851d
MD <md@doulis.com>
2014-12-30 19:49:56 +0100

    https

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var https = require('https').Server(app);
+var io = require('socket.io')(https);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -12,6 +12,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(8080, function(){
+https.listen(8080, function(){
   console.log('listening on *:8080');
 });
