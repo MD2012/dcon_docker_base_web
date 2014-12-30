e1ff863789cf9ea8b71b84fa08e8485b2cc8089f
MD <md@doulis.com>
2014-12-30 20:22:03 +0100

    http cluster

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-var app = require('express')();
-var https = require('https').Server(app);
-var io = require('socket.io')(https);
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+if (cluster.isMaster) {
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
   });
-});
+} else {
+  // Workers can share any TCP connection
+  // In this case its a HTTP server
 
-https.listen(8080, function(){
-  console.log('listening on *:8080');
-});
+  var app = require('express')();
+
+  var http = require('http').Server(app);
+  // for https proceed with:
+  // https://www.digitalocean.com/community/tutorials/openssl-essentials-working-with-ssl-certificates-private-keys-and-csrs
+  // openssl.org
+  // startssl.com
+  // https://shaaaaaaaaaaaaa.com/
+
+  var io = require('socket.io')(http);
+
+  app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+  });
+
+  io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      io.emit('chat message', msg+ ' (served by:'+cluster.worker.id+')');
+    });
+  });
+
+  http.listen(8080, function(){
+    console.log('listening on *:8080');
+  });
+}
+
