aa69e0691dfe3d21910801c03e2d1e0bc24283a8
MD <md@doulis.com>
2015-01-01 18:24:53 +0100

    nginx port config

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ if (cluster.isMaster) {
   });
 
   io.on('connection', function(socket){
+    socket.on('connect', function() {
+       io.emit('chat message', 'Another user has connected.');
+    });
     socket.on('chat message', function(msg){
       io.emit('chat message', msg+ ' (served by:'+cluster.worker.id+')');
     });
