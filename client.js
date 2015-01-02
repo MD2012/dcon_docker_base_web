14e627c05907bacb021f5bd40298f071247687f4
MD <md@doulis.com>
2015-01-02 18:03:47 +0100

    chat config

--- client.js
+++ client.js
@@ -229,7 +229,7 @@ $(function() {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "Welcome to the KIMUNDO Chat";
     log(message, {
       prepend: true
     });
