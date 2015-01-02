5848dacf1106886affe3d7ea60febc943ff1c2e1
MD <md@doulis.com>
2015-01-02 21:05:24 +0100

    mongodb connection

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 var cluster = require('cluster');
 var numCPUs = require('os').cpus().length;
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/kimdb';
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+  db.close();
+});
 
 
 // usernames which are currently connected to the chat
