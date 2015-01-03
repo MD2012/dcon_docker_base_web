5aa5b7cd2c8562b0c4fb4f498b17439cf494e7f6
MD <md@doulis.com>
2015-01-03 01:25:09 +0100

    fig build nocache

--- index.js
+++ index.js
@@ -22,8 +22,15 @@ var getMsgs = function(db) {
 // usernames which are currently connected to the chat
 var usernames = {};
 var numUsers = 0;
+var mdb = null;
 
 if (cluster.isMaster) {
+
+  // Use connect method to connect to the Server
+  MongoClient.connect(url, function(err, db) {
+    console.log("Connected correctly to server");
+    mdb = db;
+  });
   // Fork workers.
   for (var i = 0; i < numCPUs; i++) {
     cluster.fork();
@@ -58,76 +65,69 @@ if (cluster.isMaster) {
     console.log('listening on *:443');
   });
 
-  // Use connect method to connect to the Server
-  MongoClient.connect(url, function(err, db) {
+  io.on('connection', function (socket) {
+    var addedUser = false;
 
-    console.log("Connected correctly to server");
-
-    io.on('connection', function (socket) {
-      var addedUser = false;
-
-      // when the client emits 'new message', this listens and executes
-      socket.on('new message', function (data) {
-        // we tell the client to execute 'new message'
-        console.log(socket.username+':'+data);
-        var msg = {
-          username: socket.username,
-          message: data
-        };
-        insertMsg(db, msg, function() {
-          socket.broadcast.emit('new message', msg);
-        });
-      });
-
-      // when the client emits 'add user', this listens and executes
-      socket.on('add user', function (username) {
-        // we store the username in the socket session for this client
-        socket.username = username;
-        // add the client's username to the global list
-        usernames[username] = username;
-        ++numUsers;
-        addedUser = true;
-        socket.emit('login', {
-          numUsers: numUsers
-          //msgs: getMsgs(db)
-        });
-        // echo globally (all clients) that a person has connected
-        socket.broadcast.emit('user joined', {
-          username: socket.username,
-          numUsers: numUsers
-        });
-      });
-
-      // when the client emits 'typing', we broadcast it to others
-      socket.on('typing', function () {
-        socket.broadcast.emit('typing', {
-          username: socket.username
-        });
-      });
-
-      // when the client emits 'stop typing', we broadcast it to others
-      socket.on('stop typing', function () {
-        socket.broadcast.emit('stop typing', {
-          username: socket.username
-        });
-      });
-
-      // when the user disconnects.. perform this
-      socket.on('disconnect', function () {
-        // remove the username from global usernames list
-        if (addedUser) {
-          delete usernames[socket.username];
-          --numUsers;
-
-          // echo globally that this client has left
-          socket.broadcast.emit('user left', {
-            username: socket.username,
-            numUsers: numUsers
-          });
-        }
+    // when the client emits 'new message', this listens and executes
+    socket.on('new message', function (data) {
+      // we tell the client to execute 'new message'
+      console.log(socket.username+':'+data);
+      var msg = {
+        username: socket.username,
+        message: data
+      };
+      insertMsg(mdb, msg, function() {
+        socket.broadcast.emit('new message', msg);
       });
     });
 
+    // when the client emits 'add user', this listens and executes
+    socket.on('add user', function (username) {
+      // we store the username in the socket session for this client
+      socket.username = username;
+      // add the client's username to the global list
+      usernames[username] = username;
+      ++numUsers;
+      addedUser = true;
+      socket.emit('login', {
+        numUsers: numUsers
+        //msgs: getMsgs(db)
+      });
+      // echo globally (all clients) that a person has connected
+      socket.broadcast.emit('user joined', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    });
+
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', function () {
+      socket.broadcast.emit('typing', {
+        username: socket.username
+      });
+    });
+
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', function () {
+      socket.broadcast.emit('stop typing', {
+        username: socket.username
+      });
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function () {
+      // remove the username from global usernames list
+      if (addedUser) {
+        delete usernames[socket.username];
+        --numUsers;
+
+        // echo globally that this client has left
+        socket.broadcast.emit('user left', {
+          username: socket.username,
+          numUsers: numUsers
+        });
+      }
+    });
   });
 
 }
